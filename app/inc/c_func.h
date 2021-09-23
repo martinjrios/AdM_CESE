29295590aa6683a3d1a3d941f1a4ee4705ee09ef
mjrios <mrios@p-lock.net>
2021-09-23 09:33:13 -0300

    Se agregan los archivos al repositorio.

---
+++ app/inc/c_func.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+
+uint32_t c_sum (uint32_t firstOperand, uint32_t secondOperand);
+void c_zeros (uint32_t *vector, uint32_t longitud);  // Ejercicio 1
+void asm_productoEscalar32 (uint32_t * vectorIn, uint32_t * vectorOut, uint32_t longitud, uint32_t escalar); // Ejercicio 2
---
+++ app/src/c_func.c
@@ -0,0 +1,13 @@
+#include "c_func.h"
+
+
+uint32_t c_sum (uint32_t firstOperand, uint32_t secondOperand)
+{
+    return (firstOperand + secondOperand);
+}
+
+// Ejercicio 1
+void c_zeros (uint32_t *vector, uint32_t longitud)
+{
+    for(uint32_t i = 0; i < longitud; i++) vector[i] = 0;
+}
